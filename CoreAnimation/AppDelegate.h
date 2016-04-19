ab18aac7d4a118f41f06dcf507085822bd84c3f2
applicationHe <apple@test.local>
2016-04-19 16:24:53 +0800

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by apple on 16/4/19.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
